479714e9b2e1aade848b3144132463f746311717
dhanushkt <dhanushkt787@gmail.com>
2019-08-23 00:04:13 +0530

    imported startup files

---
+++ login/accesscontroladmin.php
@@ -0,0 +1,8 @@
+<?php
+include_once '../access/connect.php';
+session_start();
+if(!(isset($_SESSION['sausername'])||isset($_SESSION['ausername'])))
+{
+	echo'<script> window.location="403.php";</script>';
+}
+?>
