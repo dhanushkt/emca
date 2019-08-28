07e8accd0cfc11ef252ed6398a46d4fbb8880f46
dhanushkt <dhanushkt787@gmail.com>
2019-08-28 16:58:52 +0530

    fixed improper last commit

--- emca-master/admin/assets/footer.php
+++
@@ -1,2 +0,0 @@
-<footer class="footer text-center">2019 &copy; OFMS - Online Fest Management System</footer>
-
--- emca-master/login/accesscontrolsuperadmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../access/connect.php';
-session_start();
-if(!(isset($_SESSION['sausername'])))
-{
-	echo'<script> window.location="403.php";</script>';
-}
-?>
